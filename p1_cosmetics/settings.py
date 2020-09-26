f5aefd2334e1015ea5135c617f499e00cfce5908
carrkarr <kcarrl@yahoo.com.mx>
2020-09-25 23:39:08 -0500

    Comit b1

--- p1_cosmetics/settings.py
+++ p1_cosmetics/settings.py
@@ -13,10 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -142,3 +141,4 @@ TEMPLATE_LOADERS = [
     'django.template.loaders.app_directories.Loader',
     'django.template.loaders.eggs.Loader',
 ]
+
